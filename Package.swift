38c474a52779bf5f16c430c038d64dcc06ab7d57
Manu W <manuel@wentenschuh.eu>
2018-09-23 19:30:19 +0200

    Updated PackageDescription - Not sure if i understand the usage of 'systemLibrary' correctly; it seems to work, but i have to make more tests...

--- Package.swift
+++ Package.swift
@@ -1,21 +1,20 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:4.2
+
 
 import PackageDescription
 
 let package = Package(
     name: "postgresql-swift",
+    swiftLanguageVersions: [.v4, .v4_2],
     products: [
         .library(name: "postgresql-swift", targets: ["postgresql-swift"]),
         .executable(name: "example-1", targets: ["example-1"]),
         .executable(name: "example-2", targets: ["example-2"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/ManuW/Clibpq", from: "0.0.3")
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .systemLibrary(
+            name: "libpq"
+        ),
         .target(
             name: "postgresql-swift",
             dependencies: []
